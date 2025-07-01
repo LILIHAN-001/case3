b87cce42bbfbf17cbe660d5671c062ab27130072
LILIHAN-001 <73207687+LILIHAN-001@users.noreply.github.com>
2025-07-01 15:12:57 +0800

    Update pycart_web.py

--- pycart_web.py
+++ pycart_web.py
@@ -140,13 +140,16 @@ if st.button("Make Prediction"):  # 如果点击了预测按钮
     plt.savefig("shap_force_plot.png", bbox_inches='tight', dpi=300)
     plt.close(fig)
     st.image("shap_force_plot.png", use_container_width=True)
-#hhhh
+
+    st.subheader("Feature importance222")
+    fig, ax = plt.subplots(figsize=(3, 2.5))
     shap.waterfall_plot(
     shap.Explanation(
         values=shap_values[:,:],  
-        base_values=explainer.expected_value, # explainer.expected_value[class_index]
+        base_values=explainer.expected_value,  
         data=features,
-        feature_names=features.columns.tolist(),matplotlib=True
+        # feature_names=features.columns.tolist(),
+        matplotlib=True
     )
 )
     plt.savefig("shap_waterfall_plot.png", bbox_inches='tight', dpi=300)
