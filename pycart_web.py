cba8ce8f1154e513fb3258cb5434adb51c72b8af
LILIHAN-001 <73207687+LILIHAN-001@users.noreply.github.com>
2025-07-01 11:16:47 +0800

    Update pycart_web.py

--- pycart_web.py
+++ pycart_web.py
@@ -61,14 +61,14 @@ if st.button("Make Prediction"):  # 如果点击了预测按钮
 
     if predicted_class == 1:  # 如果预测为电复律治疗
         advice = (
-            f"According to our model, you may require electrical cardioversion. "
+            f"**Recommendation:** According to our model, you may require electrical cardioversion. "
             f"The probability of needing electrical cardioversion is {probability:.1f}%. "
             "This suggests that you may have a higher risk of requiring this treatment. "
             "I recommend consulting with a cardiologist for further examination and possible treatment options."
         )  # 如果预测为需要电复律，给出相关建议
     else:  # 如果预测为不需要电复律
         advice = (
-            f"According to our model, you do not require electrical cardioversion. "
+            f"**Recommendation:**According to our model, you do not require electrical cardioversion. "
             f"The probability of not needing electrical cardioversion is {probability:.1f}%. "
             "However, it is still important to continue regular monitoring of your heart health. "
             "Please ensure you maintain a healthy lifestyle and seek medical attention if needed."
