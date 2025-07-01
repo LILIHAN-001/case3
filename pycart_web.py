53993bee10b4b1f6016d05020e104d61efbce49d
LILIHAN-001 <73207687+LILIHAN-001@users.noreply.github.com>
2025-07-01 15:38:25 +0800

    Update pycart_web.py

--- pycart_web.py
+++ pycart_web.py
@@ -20,16 +20,16 @@ model = load_model('best_model')  # 加载训练好的ET模型
 
 # Define the feature options
 PROM_type_options = {
-    0: 'PROM>18h',  # 发作性
-    1: 'PROM<18h'  # 持续性
+    0: 'PROM>18h',  
+    1: 'PROM<18h'  
 }
 MRI_type_options = {
-    0: 'No MRI',  # 发作性
-    1: 'With MRI'  # 持续性
+    0: 'No MRI',  
+    1: 'With MRI'  
 }
 
 # Streamlit UI
-st.title("Electrical Cardioversion Predictor")  # 电复律预测器
+st.title("Early Diagnosis Predictor")  
 
 image =Image.open("Snipaste_2025-07-01_13-45-35.png")
 st.image(image )
@@ -39,13 +39,13 @@ st.image(image )
 st.sidebar.header("Input Sample Data")  # 侧边栏输入样本数据
 
 # Systolic BP input
-CRP = st.sidebar.number_input("CRP:", min_value=0, max_value=50, value=1)  # 收缩压输入框
+CRP = st.sidebar.number_input("CRP:", min_value=0, max_value=50, value=10)  # 收缩压输入框
 
 # Age input
-IL6 = st.sidebar.number_input("IL6:", min_value=0, max_value=800, value=8)  # 年龄输入框
+IL6 = st.sidebar.number_input("IL6:", min_value=0, max_value=800, value=80)  # 年龄输入框
 
 # AST input
-S100B = st.sidebar.number_input("S100B:", min_value=0, max_value=500, value=1)  # AST输入框
+S100B = st.sidebar.number_input("S100B:", min_value=0, max_value=500, value=100)  # AST输入框
 
 # AtrialFibrillationType input
 PROM = st.sidebar.selectbox("PROM_type:", options=list(PROM_type_options.keys()), format_func=lambda x: PROM_type_options[x])  # 心房颤动类型选择框
@@ -61,7 +61,7 @@ features = pd.DataFrame([feature_values],columns= ["PROM", "S100B", "IL6", "MRI"
 
 if st.button("Make Prediction"):  # 如果点击了预测按钮
     # Predict the class and probabilities
-    predicted_class = predict_model(model, raw_score = True, data=features)["prediction_label"][0] # 预测电复律结果
+    predicted_class = predict_model(model, raw_score = True, data=features)["prediction_label"][0] # 预测结果
     predicted_proba = predict_model(model, raw_score = True, data=features)[["prediction_score_0","prediction_score_1"]].values[0]
     # model.predict_proba(features)[0]  # 预测各类别的概率
 
@@ -75,8 +75,8 @@ if st.button("Make Prediction"):  # 如果点击了预测按钮
 
     # Visualize the prediction probabilities
     sample_prob = {
-        'No Electrical Cardioversion': predicted_proba[0],  # 不需要电复律的概率
-        'Needs Electrical Cardioversion': predicted_proba[1]  # 需要电复律的概率
+        'No Disease Occurrence': predicted_proba[0],  # 疾病不发生的概率
+        'Disease Occurrence': predicted_proba[1]  # 疾病发生的概率
     }
     
     VSpace(20)
@@ -85,16 +85,16 @@ if st.button("Make Prediction"):  # 如果点击了预测按钮
     plt.rc('ytick', labelsize=8) # 设置所有Y轴刻度的字体大小
     plt.rc('xtick', labelsize=8) # 设置所有X轴刻度的字体大小
     # Create bar chart
-    bars = plt.barh(['No Electrical Cardioversion', 'Needs Electrical Cardioversion'], 
-                    [sample_prob['No Electrical Cardioversion'], sample_prob['Needs Electrical Cardioversion']], 
+    bars = plt.barh(['No Disease Occurrence', 'Disease Occurrence'], 
+                    [sample_prob['No Disease Occurrence'], sample_prob['Disease Occurrence']], 
                     height=0.6, edgecolor="black", color=['#81abd3','#fcd6d3'])  # 绘制水平条形图
 
     # Add title and labels, set font bold and increase font size
-    plt.title("Prediction Probability for Electrical Cardioversion", fontsize=12, fontweight='bold')  # 添加图表标题，并设置字体大小和加粗
+    plt.title("Prediction Probability for Disease Occurrence", fontsize=12, fontweight='bold')  # 添加图表标题，并设置字体大小和加粗
     plt.xlabel("Probability", fontsize=10 )  # 添加X轴标签，并设置字体大小和加粗
 
     # Add probability text labels, adjust position to avoid overlap, set font bold
-    for i, v in enumerate([sample_prob['No Electrical Cardioversion'], sample_prob['Needs Electrical Cardioversion']]):  # 为每个条形图添加概率文本标签
+    for i, v in enumerate([sample_prob['No Disease Occurrence'], sample_prob['Disease Occurrence']]):  # 为每个条形图添加概率文本标签
         plt.text(v + 0.001, i, f"{v:.2f}", va='center', fontsize=8, color='black' )  # 设置标签位置、字体加粗
 
     # Hide other axes (top, right, bottom)
@@ -105,20 +105,20 @@ if st.button("Make Prediction"):  # 如果点击了预测按钮
     st.pyplot(plt, use_container_width=True)  # 显示图表
     
 
-    if predicted_class == 1:  # 如果预测为电复律治疗
+    if predicted_class == 1:  # 如果预测为疾病发生，给出相关建议
         advice = (
             f"**Recommendation:** According to our model, you may require electrical cardioversion. "
             f"The probability of needing electrical cardioversion is {probability:.1f}%. "
             "This suggests that you may have a higher risk of requiring this treatment. "
             "I recommend consulting with a cardiologist for further examination and possible treatment options."
-        )  # 如果预测为需要电复律，给出相关建议
-    else:  # 如果预测为不需要电复律
+        )  
+    else:  # 如果预测为不需要疾病低风险
         advice = (
             f"**Recommendation:** According to our model, you do not require electrical cardioversion. "
             f"The probability of not needing electrical cardioversion is {probability:.1f}%. "
             "However, it is still important to continue regular monitoring of your heart health. "
             "Please ensure you maintain a healthy lifestyle and seek medical attention if needed."
-        )  # 如果预测为不需要电复律，给出相关建议
+        )  
 
     st.write(advice)  # 显示建议
     
